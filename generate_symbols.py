6c8a0a4640929e32b03973b3fd9548592784b3d0
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2025-02-03 17:16:29 +0900

    fix: 記号に用いるカナ文字の範囲を修正

--- generate_symbols.py
+++ generate_symbols.py
@@ -5,8 +5,8 @@ def generate_symbols():
     digits = [str(i) for i in range(10)]
     uppercase = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
     lowercase = [chr(i) for i in range(ord('a'), ord('z') + 1)]
-    hiragana = [chr(i) for i in range(ord('あ'), ord('ん') + 1)]
-    katakana = [chr(i) for i in range(ord('ア'), ord('ン') + 1)]
+    hiragana = [chr(i) for i in range(ord('ぁ'), ord('ん') + 1)]
+    katakana = [chr(i) for i in range(ord('ァ'), ord('ヶ') + 1)]
     hankakukana = [chr(i) for i in range(ord('ｱ'), ord('ﾝ') + 1)]
     kanji = [chr(i) for i in range(ord('一'), ord('鿿') + 1)] 
     
